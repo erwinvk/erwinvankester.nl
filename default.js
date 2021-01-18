539553a62f1b1271213f6800ac62bbc2adb4b89f
erwinvk <31625952+erwinvk@users.noreply.github.com>
2021-01-18 23:56:12 +0100

    lastfm & updates

--- default.js
+++ default.js
@@ -30,7 +30,34 @@ function init_magnificPopup() {
         );
 }
 
+function setEmail() {
+    var email = $('.emailaddress').text();
+    email = email.replace('[at]', '@');
+    email = email.replace('[dot]', '.');
+    $('.emailaddress').attr('href', 'mailto:' + email);
+    $('.emailaddress').text(email);
+}
+
+/*function getArtistImage(name) {
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spotify.com/v1/search?q=Radiohead&type=artist&market=NL&limit=2&offset=5",
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer BQAXaTai88fb8Qw0p82Zf4tRTmuezjnl_XRK3LeazmgehPFs6w7p23gPDjW-Ziti73QpP-ZH2qKYYXq_qYbtiM5pDOIuEPsn05mXuS7DfHAweZ6-IWooteBqWE37H6pjgBN3VuTVs20",
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        debugger;
+    });
+}*/
+
 $(document).ready(function () {
+    setEmail();
+
 	$.get( "https://graph.instagram.com/me/media?access_token=IGQVJWNDUzLUxYN0FoQkRYRS03RXpWWjYxaHFnNmo1VG4wV2lFR1VnMUNobVFxcWRhWnlKVWZAWTkxMVy1RVzhtT0dWX3l1RTNjbnZAzcXNFU2QxRk50OWNiTlMzUGgxeWxoNHRqbHVnLUdqZAFVEX2VfNQZDZD&fields=media_url,media_type,caption,permalink", function( data ) {
 		for (var i = 0; i < 10; i++)
 	    {
@@ -38,5 +65,24 @@ $(document).ready(function () {
         }
 
         init_magnificPopup();
+    });
+
+    //get lastFM scrobble data
+    $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=erwinvk&period=3month&api_key=78026a5c9cda529f9559d9e3d8c78695&limit=10&format=json&callback=?', function (data) {
+        var artistTop10 = data.topartists.artist;
+        var html = '';
+        for (var i = 0; i < artistTop10.length; i++) {
+            var imgArray = artistTop10[i].image;
+            for (var j = 0; j < imgArray.length; j++) {
+                if (imgArray[j].size == 'large') {
+                    html += '<li data-mbid="' + artistTop10[i].mbid + '">' + artistTop10[i].name + ' ' + artistTop10[i].playcount + '</li>';
+
+                    //getArtistImage(artistTop10[i].name);
+                }
+            }
+        }
+
+        $('#lastfmtop10').html(html);
+        
     });
 });
